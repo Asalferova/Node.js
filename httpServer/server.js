0db9cf274a6dcb565aec4c8fc3a2294a4780811f
Alina Alferova <asalferova99@gmail.com>
2023-12-18 03:42:49 +0800

    node.js http-server

---
+++ httpServer/server.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const http = require("http");
+let counter = 0;
+let counterAbout = 0;
+
+const server = http.createServer((req, res) => {
+  console.log("запрос получен");
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html; charset=UTF-8" });
+    counter++;
+    res.end(`<h1>Корневая страница</h1>
+    <br>
+    <p>просмотров: ${counter}</p>
+    <br>
+    <a href="/about">Ссылка на страницу /about</a>
+    `);
+  } else if (req.url === "/about") {
+    counterAbout++;
+    res.writeHead(200, { "Content-Type": "text/html; charset=UTF-8" });
+    res.end(`<h1>Страница About</h1>
+    <br>
+    <p>просмотров: ${counterAbout}</p>
+    <br>
+    <a href="/">Ссылка на страницу /</a>
+    `);
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+    res.end("<h1>Страница не найдена</h1>");
+  }
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Сервер загружен на порту ${port}`);
+});
